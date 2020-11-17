aeffbfc0a13a9ae73b87a86fe326e886454ed447
sadlerb <sadlerb22@gmail.com>
2020-11-17 16:17:40 -0500

    Compleated SQL

--- world.php
+++ world.php
@@ -7,12 +7,53 @@ $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 $country = $_GET["country"];
-$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+$context = $_GET["context"];
+if ($context != "cities"){
+  $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+}else{
+  $stmt = $conn->query("SELECT cities.name, cities.district,cities.population FROM countries INNER JOIN cities ON countries.code = cities.country_code WHERE countries.name LIKE '%$country%'");
+}
+
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+
+
+
 ?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
+<table>
+<?php
+if ($context != "cities"){
+  findCountries($results);
+}else{
+  findCities($results);
+}
+?>
+</table>
+
+
+<?php function findCountries($results){
+  echo"<tr>
+  <th>Name</th>
+  <th>Continent</th>
+  <th>Independence</th>
+  <th>Head of State</th>
+</tr>";
+foreach ($results as $row):?>
+<tr><?= "<td>".$row['name']. "</td>" . "<td>" . $row['continent'] . "</td>" . "<td>" . $row['independence_year'] . "<td>" . $row['head_of_state']  ; ?></tr>
+<?php endforeach;
+
+  };?>
+
+
+<?php function findCities($results){
+  echo"<tr>
+  <th>Name</th>
+  <th>District</th>
+  <th>Population</th>
+</tr>";
+foreach ($results as $row):?>
+<tr><?= "<td>".$row['name']. "</td>" . "<td>" . $row['district'] . "</td>" . "<td>" . $row['population'] . "<td>";?></tr>
+<?php endforeach;
+
+  };?>
+
